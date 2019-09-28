5fbab01f6ac943d47ee00d4a13e3dbb837ba1a67
LaurenceM2015 <laurencemalonga@hotmail.com>
2019-09-28 17:19:09 +0100

    First initial commit.

---
+++ JavaScriptProjects/TodoListJS/script.js
@@ -0,0 +1,22 @@
+// Creating an empty arrays.
+var todos = ["Buy New Turtle"];
+ // Ask for new input
+ var input = prompt("What would you like to do?");
+
+// Todos while loop
+while(input !== "quit"){
+    // List all the todo tasks
+    if(input === "list") {
+        console.log(todos);
+
+    } else if (input === "new") {
+        // Ask for new todo
+        var newTodo = prompt("Enter New Todo");
+        // add to todos array
+        todos.push(newTodo);
+    }
+    // Ask again for new input
+    var input = prompt("What would you like to do ?");
+}
+console.log("ok, You Quit The App");
+
