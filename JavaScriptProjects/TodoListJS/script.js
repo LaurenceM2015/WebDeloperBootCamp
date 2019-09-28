9b9346cea285d0c59a3d7dc4b0de90a2f95977cc
LaurenceM2015 <laurencemalonga@hotmail.com>
2019-09-28 20:32:58 +0100

    Delete Todo in the list. And Refactoring the code, by creating seprate function.

--- JavaScriptProjects/TodoListJS/script.js
+++ JavaScriptProjects/TodoListJS/script.js
@@ -7,30 +7,46 @@ var todos = ["Buy New Turtle"];
 while(input !== "quit"){
     // List all the todo tasks
     if(input === "list") {
-        console.log("***********");
-        // forEach
-        todos.forEach(function(todo, i){
-            console.log(i + ": " + todo);
-        });
-        console.log("***********");
+        listTodos();
        
     } else if (input === "new") {
-        // Ask for new todo
-        var newTodo = prompt("Enter New Todo");
-        // add to todos array
-        todos.push(newTodo);
-        console.log("Added Todo");
+       addTodo();
 
     } else if(input === "delete"){
-        // ask for index of todo to be deleted
-        var index = prompt("Please enter index of todo to delete");
-        // delete that todo
-        //splice()
-        todos.splice(index,1);
-        console.log("Delete Todo" + index + " " );
+        deleteTodo();
     }
     // Ask again for new input
     var input = prompt("What would you like to do ?");
 }
 console.log("ok, You Quit The App");
 
+
+// REFACTORING THE CODE
+
+function listTodos(){
+    console.log("***********");
+    // forEach
+    todos.forEach(function(todo, i){
+        console.log(i + ": " + todo);
+    });
+    console.log("***********");
+}
+
+// ADD TODOs
+
+function addTodo(){
+    // Ask for new todo
+    var newTodo = prompt("Enter New Todo");
+    // add to todos array
+    todos.push(newTodo);
+    console.log("Added Todo");
+}
+
+function deleteTodo(){
+    // ask for index of todo to be deleted
+    var index = prompt("Please enter index of todo to delete");
+    // delete that todo
+    //splice()
+    todos.splice(index,1);
+    console.log("Delete Todo" + index + " " );
+}
