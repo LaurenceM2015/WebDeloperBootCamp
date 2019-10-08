124417124b448fe2a8bbdc71fbea4f9d255f8c88
LaurenceM2015 <laurencemalonga@hotmail.com>
2019-10-08 13:31:50 +0100

    Set the score keeper state, and reset functionality.

--- JavaScriptProjects/scoreKeeper/scorekeeper.js
+++ JavaScriptProjects/scoreKeeper/scorekeeper.js
@@ -8,17 +8,46 @@ let p2Display = document.querySelector('#p2Display');
 let p2Button = document.getElementById('p2');
 let p2Score = 0;
 
+// Game changing state
+let resetButton = document.getElementById("reset");
+let gameOver = false;
+let winningScore = 5;
+
 
 p1Button.addEventListener("click", function(){
     // To add 1 to p1Score
-    p1Score++;
-    console.log(p1Score);
-    p1Display.textContent = p1Score;
+    if(!gameOver){
+        p1Score++;
+        if(p1Score === winningScore){
+          p1Display.classList.add('winner');
+          gameOver = true;   
+        }
+        p1Display.textContent = p1Score;
+    } 
 });
 
 p2Button.addEventListener("click", function(){
     // To add 1 to p1Score
-    p2Score++;
-    console.log(p2Score);
-    p2Display.textContent = p2Score;
+    if(!gameOver){
+        p2Score++;
+        if(p2Score === winningScore){
+          p2Display.classList.add('winner');
+          gameOver = true;   
+        }
+        p2Display.textContent = p2Score;
+    } 
 });
+
+resetButton.addEventListener('click', function(){
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+
+    p1Display.classList.remove('winner');
+    p2Display.classList.remove('winner');
+
+    // Reset game
+    gameOver = false;
+    
+})
